fca8ba5085b3d07e49dd0cc2ca5642982277563e
Dot-click <129838150+Dot-Click@users.noreply.github.com>
2025-08-15 23:16:16 +0500

    added route -- Wati order

--- src/routes/wati.route.ts
+++ src/routes/wati.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getBranchesForWati,
   getMenuForWati,
+  placeOrderForWati,
 } from "@/controllers/wati.controller";
 
 const watiRoute = Router();
 
-watiRoute.get("/fetch-branches/wati", getBranchesForWati);
-watiRoute.post("/fetch-menu/wati", getMenuForWati);
+watiRoute.get("/fetch-branches", getBranchesForWati);
+watiRoute.post("/fetch-menu", getMenuForWati);
+watiRoute.post("/place-order", placeOrderForWati);
 
 export { watiRoute };
